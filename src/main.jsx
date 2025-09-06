e299e9fcbc12fdbbe341341715b02230c88634ca
LRoseline <amb17@naver.com>
2025-09-06 22:52:37 +0900

    All Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import Calendar from './Calendar.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+  <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Calendar />} />
+      <Route path="*" element={<Calendar />} />
     </Routes>
   </BrowserRouter>
 )
